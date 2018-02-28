b7b1d003186a07151ce3442ce79731215f552339
Radislav Sinicin <radik200058@gmail.com>
2018-02-28 10:39:27 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+
+import  java.lang.String;
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here ^^ goodluck
+
+        System.out.println("Hello World again!");
+        System.out.println("Hello World!");
+
+        // byte ot 127 do -128
+        // int mnogo
+        // long mnogo X2
+
+        byte num=23;
+        int num1=31548;
+        long num2=64689879;
+        float num3=23.44f; // float need "f"
+        double num4=77.555431328; // float X2=double
+        boolean isTrue=true; // TRUE=1
+        boolean isFalse=false;// FALSE=0
+        char sym='R';
+
+        String str="CTS khPI";
+
+        System.out.println(str);
+        System.out.println(isFalse);
+        System.out.println(num+num3+num4);
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
